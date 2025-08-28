b03b9fbaf8b2d7d736bee1fcb20be242f0ef94d5
HarumaIto <itoharm@gmail.com>
2025-08-28 15:34:14 +0900

    update: taskがある場合の処理を関数化して、最後にもう一度実行する

--- main.py
+++ main.py
@@ -21,18 +21,21 @@ def get_bme280_data() -> dict[str, float]:
 def get_tsl2572_data() -> dict[str, float]:
     return tsl2572_sample.readData()
 
+def check_response_play_audio():
+    for task_id in task_ids:
+        response = get_task(task_id)
+        if response["status"] == "completed":
+            # ここに再生処理を実装
+            print(f"Task {task_id} completed. Result: {response['result']}")
+            task_ids.remove(task_id)
+
 def main():
     bme280_sample.init()
     tsl2572_sample.init()
 
     for _ in range(3):
         if len(task_ids) != 0:
-            for task_id in task_ids:
-                response = get_task(task_id)
-                if response["status"] == "completed":
-                    # ここに再生処理を実装
-                    print(f"Task {task_id} completed. Result: {response['result']}")
-                    task_ids.remove(task_id)
+            check_response_play_audio()
 
         record_audio(5, WAVE_OUTPUT_FILENAME)
         wav_to_mp3()
@@ -41,6 +44,8 @@ def main():
 
         response = post_data(MP3_OUTPUT_FILENAME, bme280_data, tsl2572_data)
         task_ids.append(response["task_id"])
+
+    check_response_play_audio()
 
 if __name__ == "__main__":
     main()
